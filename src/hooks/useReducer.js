504f86098f0985592ad5df76b5b1d479d507fcfa
uladzimir <vova108vova@gmail.com>
2023-06-11 13:00:42 +0300

    useReducer, change state in switch with init function

--- src/hooks/useReducer.js
+++ src/hooks/useReducer.js
@@ -1,30 +1,33 @@
 import { useReducer } from 'react'
 
 const UseReducerComponent = () => {
-  const [state, dispatch] = useReducer(reducer, {count: 0}, init)
+  const [state, dispatch] = useReducer(reducer, {count: 0})
   function reducer(state, { type, payload = 100 }) {
     switch (type) {
       case 'plus': return { ...state, count: state.count + payload }
       case 'minus': return { ...state, count: state.count - payload }
+      case 'reset': return init()
       default: throw new Error()
     }
   }
   console.log(state)
   function init(state) {
-    return {...state, theme: 'dark'}
+    return {...state, theme: 'dark', count: 0}
   }
   const styles = {
     div: {
       display: 'flex',
       flexDirection: 'column',
       gap: '20px',
+      backgroundColor: state.theme ? 'grey' : 'white'
     }
   }
   return (
     <div style={styles.div}>
       <div>Rezult: {state.count}</div>
       <button onClick={() => dispatch({type: 'plus', payload: 200})}>Plus 100 or 200</button>
-      <button onClick={() => dispatch({ type: 'minus', payload: 200})}>Minus 100 or 200</button>
+      <button onClick={() => dispatch({ type: 'minus', payload: 200 })}>Minus 100 or 200</button>
+      <button onClick={() => dispatch({ type: 'reset'})}>Reset and add dark theme</button>
     </div>
   )
 }

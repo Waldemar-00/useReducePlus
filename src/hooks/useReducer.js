3fe6432582714d702a9f86d5cecdacb13eed82f3
uladzimir <vova108vova@gmail.com>
2023-06-11 13:13:09 +0300

    dispatched payload as variable to init function

--- src/hooks/useReducer.js
+++ src/hooks/useReducer.js
@@ -6,20 +6,20 @@ const UseReducerComponent = () => {
     switch (type) {
       case 'plus': return { ...state, count: state.count + payload }
       case 'minus': return { ...state, count: state.count - payload }
-      case 'reset': return init()
+      case 'reset': return init(payload)
       default: throw new Error()
     }
   }
   console.log(state)
-  function init(state) {
-    return {...state, theme: 'dark', count: 0}
+  function init(payload, state) {
+    return {...state, theme: payload, count: 0}
   }
   const styles = {
     div: {
       display: 'flex',
       flexDirection: 'column',
       gap: '20px',
-      backgroundColor: state.theme ? 'grey' : 'white'
+      backgroundColor: state.theme ? state.theme : 'white'
     }
   }
   return (
@@ -27,7 +27,7 @@ const UseReducerComponent = () => {
       <div>Rezult: {state.count}</div>
       <button onClick={() => dispatch({type: 'plus', payload: 200})}>Plus 100 or 200</button>
       <button onClick={() => dispatch({ type: 'minus', payload: 200 })}>Minus 100 or 200</button>
-      <button onClick={() => dispatch({ type: 'reset'})}>Reset and add dark theme</button>
+      <button onClick={() => dispatch({ type: 'reset', payload: 'grey'})}>Reset and add dark theme</button>
     </div>
   )
 }
